35a2e23f32c40be72a8c87ac1bf1a14083b25084
Paul Borawski <paulborawski14@gmail.com>
2018-02-20 08:24:24 -0800

    using jwt to make an encrypted web token

--- server/models/user.js
+++ server/models/user.js
@@ -9,15 +9,20 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre('save', function(next) {
+  // get access to the user model
   const user = this;
 
+  // generate a salt then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) { return next(err); }
 
+    // hash password using the salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) { return next(err); }
 
+      // overwrite plain text password with encrypted password
       user.password = hash;
       next();
     })

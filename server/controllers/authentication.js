f317aa7622062bbd852e6bc1376ae284aebc21c7
Paul Borawski <paulborawski14@gmail.com>
2018-02-19 17:26:32 -0800

    checking for existing user and creating a new user

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,29 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true'});
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+
+  // If a user with email does exist, return an Error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use' });
+    }
+
+  // If a user with email does not exist, create and save user record
+  const user = new User({
+    email: email,
+    password: password
+  });
+
+  user.save(function(err) {
+    if (err) { return next(err); }
+
+  // Respond to request indicating the user was created
+  res.json(user);
+  });
+ });
 }

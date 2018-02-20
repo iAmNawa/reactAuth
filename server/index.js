719563ca45e59e087db3461dd670ee81310e805f
Paul Borawski <paulborawski14@gmail.com>
2018-02-19 16:22:42 -0800

    server making a connection with mongodb instance

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router.js');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:27017/auth');
 
 // App Setup
 app.use(morgan('combined'));

1eb1e3e39076b6e52d5ddb39f2286251d0ebaaca
Paul Borawski <paulborawski14@gmail.com>
2018-02-19 16:42:57 -0800

    authentication route working on postman localhost:3449/signup

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
-  app.get('/', function(req, res, next) {
-    res.send(['json', 'is', 'working']);   
-  });
+  app.post('/signup', Authentication.signup);
 }

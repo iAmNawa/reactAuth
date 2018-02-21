f9ba86942e10af2f31dbc341f59250873376f954
Paul Borawski <paulborawski14@gmail.com>
2018-02-20 21:02:58 -0800

    FULLY WORKING back-end authorization - front-end is next

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,27 @@ const User = require ('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local Strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  // Verify this username and password, call done with the user
+  // if it is the correct email and password
+  // otherwise, call done with false
+  User.findOne({ email: email}, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) {return done(null, false); }
+
+    // compare passwords - is 'password' equal to user.password?
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+});
 
 // Setup options for JWT Strategy
 const jwtOptions = {
@@ -28,3 +49,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);

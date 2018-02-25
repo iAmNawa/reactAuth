f56448d61a738e3a8b093acf0813b7f62b5f974b
Paul Borawski <paulborawski14@gmail.com>
2018-02-25 10:02:15 -0800

    adding redux-thunk middleware

--- reactFront/README.md
+++ reactFront/README.md
@@ -1,3 +1,11 @@
 # ReduxSimpleStarter
 
 This is the simple react starter that will be used for the front-end
+
+database - mongo - in terminal type mongod to run
+
+
+back-end server - type npm run dev in the server directory
+
+
+front-end server - webpack-dev-server - npm run start in reactFront directory
--- reactFront/src/actions/index.js
+++ reactFront/src/actions/index.js
@@ -1,13 +1,16 @@
 
+const ROOT_URL = 'http://localhost:3449';
 
 export function signinUser({ email, password }) {
-  // Submit email/password to the server
+  return function(dispatch) {
+    // Submit email/password to the server
 
-  // If request is good...
-  // - Update state to indicate user is authernticated
-  // - Save the JWT token
-  // -redirect to the route '/feature'
+    // If request is good...
+    // - Update state to indicate user is authernticated
+    // - Save the JWT token
+    // -redirect to the route '/feature'
 
-  // If request is bad...
-  // - Show an error to the user
+    // If request is bad...
+    // - Show an error to the user
+  }
 }

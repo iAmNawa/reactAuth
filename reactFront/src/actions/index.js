94ba2d89f138a67aafd7eaa0cef5c6dd392a6280
Paul Borawski <paulborawski14@gmail.com>
2018-02-25 16:57:53 -0800

    adding a promise that will send user to /feature if the sign in is authenticated

--- reactFront/src/actions/index.js
+++ reactFront/src/actions/index.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3449';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
     // Submit email/password to the server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-
-    // If request is good...
-    // - Update state to indicate user is authernticated
-    // - Save the JWT token
-    // -redirect to the route '/feature'
-
-    // If request is bad...
-    // - Show an error to the user
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
+        // - redirect to the route '/feature'
+         browserHistory.push('/feature');
+      })
+      .catch()
+        // If request is bad...
+        // - Show an error to the user
   }
 }

839a10dcf09ceeee405bdc0174155c12b575d0b4
Paul Borawski <paulborawski14@gmail.com>
2018-02-27 16:36:44 -0800

    signup logic scaffolding

--- reactFront/src/components/auth/signup.js
+++ reactFront/src/components/auth/signup.js
@@ -4,10 +4,29 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
   render() {
+    const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
+
     return (
-      <div>Signup</div>
+      <form>
+       <fieldset className="form-group">
+        <label>Email:</label>
+        <input className="form-control" {...email} />
+       </fieldset>
+       <fieldset className="form-group">
+        <label>Password:</label>
+        <input className="form-control" {...password} />
+       </fieldset>
+       <fieldset className="form-group">
+        <label>Confirm Password:</label>
+        <input className="form-control" {...passwordConfirm} />
+       </fieldset>
+       <button action="submit" className="btn btn-primary">Sign up</button>
+      </form>
     );
   }
 }
 
-export default Signup;
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm']
+})(Signup);

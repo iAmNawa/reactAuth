5b744443757f86defaa0f8060660ae4613b26412
Paul Borawski <paulborawski14@gmail.com>
2018-03-04 19:26:45 -0800

    delete error messages on successful login

--- reactFront/src/reducers/auth_reducer.js
+++ reactFront/src/reducers/auth_reducer.js
@@ -7,11 +7,11 @@ import {
 export default function(state = {}, action) {
   switch(action.type) {
     case AUTH_USER:
-      return { ...state, authenticated: true };
+      return { ...state, error: '', authenticated: true };
     case UNAUTH_USER:
       return { ...state, authenticated: false };
     case AUTH_ERROR:
-      return { ...state, error: action.payload };    
+      return { ...state, error: action.payload };
   }
 
 

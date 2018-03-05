d80e105d225b28b682df4015892e2d853ee04a47
Paul Borawski <paulborawski14@gmail.com>
2018-03-05 09:50:48 -0800

    keeping user logged in as long as token exists

--- reactFront/src/index.js
+++ reactFront/src/index.js
@@ -13,11 +13,21 @@ import Feature from './components/auth/feature';
 import RequireAuth from './components/auth/require_auth';
 import Welcome from './components/welcome';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If we have a token, the user is signed in
+if (token) {
+  // We need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
+
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Welcome} />
